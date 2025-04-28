0dc2fd665b97151d00bac6249f172418fda8a1ba
신현식 <hunsik777@gmail.com>
2025-04-28 10:49:58 +0900

    validator 적용

--- router/posts.mjs
+++ router/posts.mjs
@@ -1,7 +1,14 @@
 import express from "express";
 import * as postController from "../controller/post.mjs";
+import { body } from "express-validator";
+import { validate } from "../middleware/validator.mjs";
 
 const router = express.Router();
+
+const validatePost = [
+  body("text").trim().isLength({ min: 5 }).withMessage("최소 5자이상 입력"),
+  validate,
+];
 
 // 모든 포스트 가져오기
 // 해당 아이디에 대한 포스트 가져오기
@@ -19,7 +26,7 @@ router.get("/:id", postController.getPost);
 // POST
 // http://127.0.0.1:8080/posts
 // json 형태로 입력 후 저장
-router.post("/", postController.createPost);
+router.post("/", validatePost, postController.createPost);
 
 // 포스트 수정하기
 // PUT
@@ -30,6 +37,6 @@ router.put("/:id", postController.updatePost);
 // 포스트 삭제하기
 // DELETE
 // http://127.0.0.1:8080/posts/:id
-router.delete("/:id", postController.deletePost);
+router.delete("/:id", validatePost, postController.deletePost);
 
 export default router;
