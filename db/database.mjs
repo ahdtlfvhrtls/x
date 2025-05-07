3f306de65642c66806165721541a454d2fd410d5
신현식 <hunsik777@gmail.com>
2025-05-07 17:47:53 +0900

    mysql-db 적용

---
+++ db/database.mjs
@@ -0,0 +1,14 @@
+import mysql from "mysql2";
+import { config } from "../config.mjs";
+
+const pool = mysql.createPool({
+  host: config.db.host,
+  user: config.db.user,
+  password: config.db.password,
+  database: config.db.database,
+});
+
+export const db = pool.promise();
+
+// module.exports = pool;
+export default pool;
