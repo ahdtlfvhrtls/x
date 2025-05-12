33e03c9223891da13018bf24f3c70480279b854d
신현식 <hunsik777@gmail.com>
2025-05-12 11:34:39 +0900

    mongoose 적용

--- data/auth.mjs
+++ data/auth.mjs
@@ -1,34 +1,31 @@
-import MongoDb from "mongodb";
-import { getUsers } from "../db/database.mjs";
+import Mongoose from "mongoose";
+import { useVirtualId } from "../db/database.mjs";
 
-const ObjectID = MongoDb.ObjectId;
+const userSchema = new Mongoose.Schema(
+  {
+    userid: { type: String, require: true },
+    name: { type: String, require: true },
+    email: { type: String, require: true },
+    password: { type: String, require: true },
+    url: String,
+  },
+  { versionKey: false }
+);
+
+useVirtualId(userSchema);
+
+const User = Mongoose.model("User", userSchema);
+// User를 넣으면 알아서 Users라고 나오기때문에 단수로 넣는다
 
 // 회원 가입
 export async function createUser(user) {
-  return getUsers()
-    .insertOne(user)
-    .then((result) => result.insertedId.toString());
-}
-
-// 로그인
-export async function login(userid, password) {
-  const user = users.find(
-    (user) => user.userid === userid && user.password === password
-  );
-  return user;
+  return new User(user).save().then((data) => data.id);
 }
 
 export async function findByUserid(userid) {
-  return getUsers().find({ userid }).next().then(mapOptionalUser);
+  return User.findOne({ userid });
 }
 
 export async function findByid(id) {
-  return getUsers()
-    .find({ _id: new ObjectID(id) })
-    .next()
-    .then(mapOptionalUser);
-}
-
-function mapOptionalUser(user) {
-  return user ? { ...user, id: user._id.toString() } : user;
+  return User.findById(id);
 }
